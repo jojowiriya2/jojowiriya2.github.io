a673cd3502cf9a9d81330fbe7036c66a8db8a2f6
jojowiriya2 <56081251+jojowiriya2@users.noreply.github.com>
2019-10-02 21:46:50 +0700

    final push

---
+++ js/projs.js
@@ -0,0 +1,48 @@
+$(window).scroll(function () {
+if ($(window).scrollTop() >= 50) 
+{
+$('.navbar').css('background','white');
+} 
+else 
+{
+$('.navbar').css('background','transparent');
+}
+});
+
+
+
+$('body').scrollspy({ target: '.navbar' });
+
+
+
+
+var deadline = new Date("OCt 3, 2019 23:00:00").getTime(); 
+var x = setInterval(function() { 
+var now = new Date().getTime(); 
+var t = deadline - now; 
+var days = Math.floor(t / (1000 * 60 * 60 * 24)); 
+var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60)); 
+var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60)); 
+var seconds = Math.floor((t % (1000 * 60)) / 1000); 
+document.getElementById("demo").innerHTML = days + "d "  
++ hours + "h " + minutes + "m " + seconds + "s "; 
+    if (t < 0) { 
+        clearInterval(x); 
+        document.getElementById("demo").innerHTML = "EXPIRED"; 
+    } 
+}, 1000); 
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 2500);    
+}
